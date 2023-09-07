30eb3e6cf3ccef3e4694a2a427757b27432aedf5
vandalizer360 <143940762+vandalizer360@users.noreply.github.com>
2023-09-07 18:55:42 -0400

    Update BGS.lua

--- BGS.lua
+++ BGS.lua
@@ -1,1 +1,21 @@
+-- made cuz i was bored and havent made a gui in awhile
+-- ALL CREDIT GOES TO ME & KAVOS LIBRARY
+-- TO CONTACT ME DM @3.60
+
+-- GUI
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("360 BGSV Auto-Hatcher", colors)
+
+--Colors
+local colors = {
+    SchemeColor = Color3.fromRGB(0,255,255),
+    Background = Color3.fromRGB(0, 0, 0),
+    Header = Color3.fromRGB(47, 12, 79),
+    TextColor = Color3.fromRGB(255,255,255),
+    ElementColor = Color3.fromRGB(20, 20, 20)
+}
+
+-- Hatching
+local Limited = Window:NewTab("Test")
+
 
